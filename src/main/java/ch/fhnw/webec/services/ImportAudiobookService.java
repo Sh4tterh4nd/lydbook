007d61a5b524111c0bd3b7556f2d826bf5122b12
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-11-17 06:16:48 +0100

    import basic mp3, read mp3 tags

--- src/main/java/ch/fhnw/webec/services/ImportAudiobookService.java
+++ src/main/java/ch/fhnw/webec/services/ImportAudiobookService.java
@@ -1,8 +1,17 @@
 package ch.fhnw.webec.services;
 
+import ch.fhnw.webec.entity.Author;
+import ch.fhnw.webec.entity.Book;
 import ch.fhnw.webec.repository.AuthorRepository;
 import ch.fhnw.webec.repository.BookRepository;
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 @Service
 public class ImportAudiobookService {
@@ -14,7 +23,25 @@ public class ImportAudiobookService {
         this.bookRepository = bookRepository;
     }
 
-    public void addAudiobook(){
+    public void addAudiobook(String originalFilename, Path file) throws InvalidDataException, IOException, UnsupportedTagException {
+        Author author = new Author();
+        Book book = new Book();
+        Mp3File mp3file = new Mp3File(file);
+        book.setLength(mp3file.getLengthInSeconds());
+        book.setFilename(file.getFileName().toString());
+        if (mp3file.hasId3v1Tag()) {
+            ID3v1 id3v1Tag = mp3file.getId3v1Tag();
+            book.setTitle(id3v1Tag.getAlbum());
+            author.setName(id3v1Tag.getArtist());
+        }
+        if (mp3file.hasId3v2Tag()) {
+            ID3v1 id3v2Tag = mp3file.getId3v2Tag();
+            book.setTitle(id3v2Tag.getAlbum());
+            author.setName(id3v2Tag.getArtist());
+        }
 
+        authorRepository.save(author);
+        book.setAuthor(author);
+        bookRepository.save(book);
     }
 }
