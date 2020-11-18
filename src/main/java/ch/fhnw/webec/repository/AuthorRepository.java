1c012ba06f7b271ee211eeb1ea770a5340116c64
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-11-18 05:36:25 +0100

    DB Repository, Author Page with images

--- src/main/java/ch/fhnw/webec/repository/AuthorRepository.java
+++ src/main/java/ch/fhnw/webec/repository/AuthorRepository.java
@@ -4,7 +4,13 @@ import ch.fhnw.webec.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AuthorRepository extends JpaRepository<Author,Long> {
+    List<Author> findAuthorsByName(String name);
+    List<Author> findAuthorsByNameIsLike(String name);
+    Author findAuthorById(Long id);
+    List<Author> findAllByOrderByNameAsc();
 //
 }
--- src/main/java/ch/fhnw/webec/services/ImportAudiobookService.java
+++ src/main/java/ch/fhnw/webec/services/ImportAudiobookService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 @Service
 public class ImportAudiobookService {
@@ -27,21 +28,29 @@ public class ImportAudiobookService {
         Author author = new Author();
         Book book = new Book();
         Mp3File mp3file = new Mp3File(file);
+        String authorName = "";
+
         book.setLength(mp3file.getLengthInSeconds());
         book.setFilename(file.getFileName().toString());
         if (mp3file.hasId3v1Tag()) {
             ID3v1 id3v1Tag = mp3file.getId3v1Tag();
             book.setTitle(id3v1Tag.getAlbum());
-            author.setName(id3v1Tag.getArtist());
+            authorName = id3v1Tag.getArtist();
         }
         if (mp3file.hasId3v2Tag()) {
             ID3v1 id3v2Tag = mp3file.getId3v2Tag();
             book.setTitle(id3v2Tag.getAlbum());
-            author.setName(id3v2Tag.getArtist());
+            authorName = id3v2Tag.getArtist();
         }
+        author.setName(authorName);
+        System.out.println("Author" + authorName);
+        List<Author> a = authorRepository.findAuthorsByName(authorName);
+
 
         authorRepository.save(author);
         book.setAuthor(author);
         bookRepository.save(book);
     }
+
+
 }
