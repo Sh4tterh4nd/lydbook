9d28ce9496b4295a2ac1911bb29f8e6f9e6fefd9
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-11-22 07:55:54 +0100

    Basics of Spring Security, Role admin for admin section, navigation based renders based on roles

---
+++ src/main/java/ch/fhnw/webec/config/SecurityConfiguration.java
@@ -0,0 +1,67 @@
+package ch.fhnw.webec.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
+
+    private final DataSource dataSource;
+
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery("SELECT username, password, true FROM users WHERE username=?")
+////                .authoritiesByUsernameQuery("SELECT username, concat('ROLE_', role) FROM users WHERE username=?")
+//                .passwordEncoder(passwordEncoder());
+
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("USER");
+    }
+
+    @Configuration
+    public static class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity
+                    .authorizeRequests()
+                    .antMatchers("/css/**","/js/**").permitAll()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .permitAll();
+        }
+
+    }
+}
