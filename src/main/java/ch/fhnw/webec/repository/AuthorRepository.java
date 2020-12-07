fb18ccee9661bf45c7916a221feb2499de9a1186
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-07 13:33:02 +0100

    AuthorRepository Custom Impl

--- src/main/java/ch/fhnw/webec/repository/AuthorRepository.java
+++ src/main/java/ch/fhnw/webec/repository/AuthorRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author,Long> {
+public interface AuthorRepository extends JpaRepository<Author,Long>, AuthorRepositoryCustom {
     List<Author> findAuthorsByName(String name);
     Author findAuthorByName(String name);
     Author findAuthorById(Long id);
