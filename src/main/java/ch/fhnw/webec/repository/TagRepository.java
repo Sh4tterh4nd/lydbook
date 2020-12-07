0fc3138c0cab6288d551550386a6bbfd5e8ddc4c
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-07 14:03:46 +0100

    Show Book Tags on Book Page

--- src/main/java/ch/fhnw/webec/repository/TagRepository.java
+++ src/main/java/ch/fhnw/webec/repository/TagRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long>, TagRepositoryCustom {
-    List<Tag> findAllByOrOrderByName();
+    List<Tag> findAllByOrderByName();
 }
