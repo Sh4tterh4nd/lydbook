f65cf671b35d20b1dd597e4cf3ce7b816ac137f1
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-11 02:55:06 +0100

    Tag for Users

--- src/main/java/ch/fhnw/webec/services/UserService.java
+++ src/main/java/ch/fhnw/webec/services/UserService.java
@@ -2,29 +2,56 @@ package ch.fhnw.webec.services;
 
 import ch.fhnw.webec.entity.User;
 import ch.fhnw.webec.entity.UserRole;
+import ch.fhnw.webec.repository.TagRepository;
 import ch.fhnw.webec.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TagRepository tagRepository;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, TagRepository tagRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.tagRepository = tagRepository;
     }
 
-
+    @Transactional
     public void createUser(User user) {
         user.setRole(UserRole.USER);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.addTag(tagRepository.findFirstByName("Audiobook"));
         userRepository.save(user);
     }
 
-
-    public void deleteUser(Long id){
+    @Transactional
+    public void deleteUser(Long id) {
         userRepository.deleteUserById(id);
+    }
+
+    @Transactional
+    public User getUser(Long id) {
+        User user = userRepository.findUserById(id);
+        user.setPassword("");
+        user.setProgressList(new ArrayList<>());
+        return user;
+    }
+
+    @Transactional
+    public void updateUser(User updatedUser) {
+        User user = userRepository.findUserById(updatedUser.getId());
+        if (!updatedUser.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        }
+        user.getTags().clear();
+        updatedUser.getTags().forEach(t -> user.addTag(t));
+
+        userRepository.save(user);
     }
 }
