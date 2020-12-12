55bba31c45a17b433584bef9cdbb7066c79ded2d
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-12 21:13:33 +0100

    Update AudiobookUtilTests

--- src/test/java/ch/fhnw/webec/util/AudiobookUtilTest.java
+++ src/test/java/ch/fhnw/webec/util/AudiobookUtilTest.java
@@ -8,7 +8,72 @@ class AudiobookUtilTest {
 
     @Test
     void getFileEnding_withMoreDots() {
-        String filename ="Test.mp3";
-        assertEquals(AudiobookUtil.getFileEnding(filename),".mp3");
+        String filename = "ABC.Test.mp3";
+        assertEquals(AudiobookUtil.getFileEnding(filename), ".mp3");
     }
+
+    @Test
+    void getFileEnding_withOneDot() {
+        String filename = "Test.mp3";
+        assertEquals(AudiobookUtil.getFileEnding(filename), ".mp3");
+    }
+
+    @Test
+    void getFileEnding_withNoDots() {
+        String filename = "Testmp3";
+        assertEquals(AudiobookUtil.getFileEnding(filename), "");
+    }
+
+    @Test
+    void isNumeric_withOnlyNumbers() {
+        String number = "1234";
+        assertTrue(AudiobookUtil.isNumeric(number));
+    }
+
+    @Test
+    void isNumeric_withDecimalNumber() {
+        String number = "12.34";
+        assertTrue(AudiobookUtil.isNumeric(number));
+    }
+
+    @Test
+    void isNumeric_withDecimalNumberDotInFront() {
+        String number = ".1234";
+        assertTrue(AudiobookUtil.isNumeric(number));
+    }
+
+    @Test
+    void isNumeric_withDecimalNumberDotInEnd() {
+        String number = "1234.";
+        assertTrue(AudiobookUtil.isNumeric(number));
+    }
+
+    @Test
+    void isNumeric_withOnlyLetters() {
+        String number = "abc";
+        assertFalse(AudiobookUtil.isNumeric(number));
+    }
+
+    @Test
+    void isNumeric_withLettersNumbersMixed() {
+        String number = "abc123";
+        assertFalse(AudiobookUtil.isNumeric(number));
+    }
+
+    @Test
+    void isNumeric_withLettersNumbersPointMixed() {
+        String number = "abc.123";
+        assertFalse(AudiobookUtil.isNumeric(number));
+    }
+    @Test
+    void isNumeric_withDecimalWithLetter() {
+        String number = "l123.4";
+        assertFalse(AudiobookUtil.isNumeric(number));
+    }
+    @Test
+    void isNumeric_withNumberWithMultiplePoints() {
+        String number = "1.23.4";
+        assertFalse(AudiobookUtil.isNumeric(number));
+    }
+
 }
