e7d4919282ce092ad3d466f1762e424b5586fadf
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-12 21:13:33 +0100

    Update AudiobookUtilTests

--- src/test/java/ch/fhnw/webec/GradedExerciseApplicationTests.java
+++ src/test/java/ch/fhnw/webec/GradedExerciseApplicationTests.java
@@ -1,6 +1,7 @@
 package ch.fhnw.webec;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
