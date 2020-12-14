7ebfd039ddcb6743183a05407c7fd518cda65aa9
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-14 18:49:49 +0100

    ProgressServiceTest added + author and book data for test

---
+++ src/test/resources/db/changelog/scripts/insert_test_books.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (id, data_name, title, author_id) VALUES (1000,	'6101cc04-e279-48d1-9178-61b43c5ba820',	'Skyward',	1000);
+INSERT INTO books (id, data_name, title, author_id) VALUES (1001,	'c39af283-7d9e-4771-93a3-28c3d77aeed5',	'In 80 Tagen um die Welt',	1001);
