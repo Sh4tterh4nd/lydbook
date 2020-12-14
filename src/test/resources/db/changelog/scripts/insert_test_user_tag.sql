ee21121b782ce24c3295eb446bfcb47c3a3520e3
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-14 17:46:11 +0100

    Index.html and insert data for tests

---
+++ src/test/resources/db/changelog/scripts/insert_test_user_tag.sql
@@ -0,0 +1,7 @@
+INSERT INTO "user_tag" ("user_id", "tag_id") VALUES (10000,	10000);
+INSERT INTO "user_tag" ("user_id", "tag_id") VALUES (10001,	10000);
+INSERT INTO "user_tag" ("user_id", "tag_id") VALUES (10002,	10000);
+INSERT INTO "user_tag" ("user_id", "tag_id") VALUES (10003,	10000);
+INSERT INTO "user_tag" ("user_id", "tag_id") VALUES (10004,	10000);
+INSERT INTO "user_tag" ("user_id", "tag_id") VALUES (10005,	10000);
+INSERT INTO "user_tag" ("user_id", "tag_id") VALUES (10006,	9952);
