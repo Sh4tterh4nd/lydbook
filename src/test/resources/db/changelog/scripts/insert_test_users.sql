210d88712037f13b02b1c45942a7d6f4763bce37
Arieh Kellermann <Arieh.Kellermann@bluewin.ch>
2020-12-14 17:46:11 +0100

    Index.html and insert data for tests

---
+++ src/test/resources/db/changelog/scripts/insert_test_users.sql
@@ -0,0 +1,6 @@
+INSERT INTO "users" ("id", "password", "role", "username") VALUES (10001,	'$2a$12$cJXevleAuFEjCxCZNVf3KupISSXhIdcFnoFE90Th80pOFUZwUi1Wi',	'USER',	'test1');
+INSERT INTO "users" ("id", "password", "role", "username") VALUES (10002,	'$2a$12$8F1hbgwf0/nfN3hYwxGCYecGtHqCbDR.8AxDvAv5VZ.KSsSV27N5m',	'USER',	'test2');
+INSERT INTO "users" ("id", "password", "role", "username") VALUES (10003,	'$2a$12$2/CYgmViTOQ8jbpc90oWKOOEgfAURAgLi8TESOMsKco7UBoDLX3wi',	'USER',	'test3');
+INSERT INTO "users" ("id", "password", "role", "username") VALUES (10004,	'$2a$12$nsd6d8jIMllV8ox81o750.OzDmPE6PCSiX6lZTtOLZW8r5FVlaYpe',	'USER',	'test4');
+INSERT INTO "users" ("id", "password", "role", "username") VALUES (10005,	'$2a$12$ElET7iYiDlrApqQU/6T8LeJhHPryCgKMkMQmZPmay7Rq0mq1I8jhu',	'USER',	'test5');
+INSERT INTO "users" ("id", "password", "role", "username") VALUES (10006,	'$2a$12$lH9T0rDeMz2RgGcLvNnclOhZaMvPnFLJiCxbPR8Z2ZOWtJ15ycjHC',	'USER',	'test6');
